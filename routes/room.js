3cf25778b3ee1de0b76ffea41c4a3389a22b894c
Tuqiri <tuqiri@tuqiri.net>
2014-05-13 17:05:45 +0100

    Added room route to deal with room management and added database connection tools

---
+++ routes/room.js
@@ -0,0 +1,30 @@
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/SharpFrame');
+
+/*
+ * GET register page.
+ */
+
+exports.register = function(req, res){
+    res.render('room-register', { title: 'Register' });
+};
+
+/*
+ * GET view page.
+ */
+
+exports.view = function(req, res){
+    res.render('room-view', { title: 'Welcome to x' });
+};
+
+/*
+ * GET rooms.
+ */
+
+exports.getrooms = function(req, res){
+    var collection = db.get("rooms");
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    })
+};
