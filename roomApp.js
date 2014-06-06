f5a04cbd7d90cd739691500f788663a3a3c0525d
Tuqiri <tuqiri@tuqiri.net>
2014-06-06 18:45:38 +0100

    Changed code

--- roomApp.js
+++ roomApp.js
@@ -79,9 +79,9 @@ io.sockets.on('connection', function (socket) {
         var values = JSON.parse(data);
 
         //Finding the room
-        rooms.forEach(function(room) {
+        rooms.forEach(function (room) {
 
-            if(String(room._id) == values.roomid) {
+            if (String(room._id) == values.roomid) {
                 console.log("Room Found!");
                 socket.room = room;
                 socket.join(room._id);
@@ -91,11 +91,11 @@ io.sockets.on('connection', function (socket) {
         });
 
         //Finding the video to the room
-        videos.forEach(function(video) {
+        videos.forEach(function (video) {
 
-            if(video.roomid == String(socket.room._id)) {
+            if (video.roomid == String(socket.room._id)) {
 
-                if(video.isplaying == 1) {
+                if (video.isplaying == 1) {
 
                     socket.video = video;
                     console.log("Found the video");
@@ -103,18 +103,72 @@ io.sockets.on('connection', function (socket) {
             }
         });
 
-        //Adding one to the current views on the video and update the database
-        socket.room.views = socket.room.views + 1;
-        socket.room.connectedUsers = io.sockets.clients(socket.room._id).length;
-        socket.room.save();
-
         //Adding the user to the socket session and broadcasting to users in the room
         socket.username = values.username;
         socket.broadcast.to(socket.room._id).emit("servermessage", socket.username + " has joined");
         socket.emit("servermessage", "Welcome to " + socket.room.title)
+    });
 
+    socket.on('getVideoTime' ,function(data){
         //Sending the video time to the connected user
         var videotimearray = { "currenttime" : Math.round((new Date()).getTime() / 1000) , "videostarttime" : socket.video.starttime }
-        socket.emit("videoTime", JSON.stringify(videotimearray));
+        socket.emit("videotime", JSON.stringify(videotimearray));
     });
+
+    setInterval(function() {
+        videos.forEach(function(video){
+            if(video.endtime < Math.round((new Date()).getTime() / 1000)){
+                if(video.isplaying != 0){
+                    console.log("change video");
+                    ChangeVideo(video);
+                }
+            }
+        });
+    }, 5000);
+
+    function ChangeVideo(video) {
+        //Ending the current playing video
+        video.isplaying = 0;
+        video.hidden = 1;
+
+        video.save();
+
+        Video.findOne({ roomid : video.roomid, hidden : 0, isplaying : 0}, function(err, data){
+            //if data is not null then videos are found and sets the video up to played if not continues with the current video
+            if(data != null) {
+                videos.forEach(function(video) {
+                    if (video.id == data.id) {
+                        console.log("Found video");
+
+                        video.isplaying = 1;
+                        video.hidden = 0;
+                        video.starttime = Math.round((new Date()).getTime() / 1000);
+                        video.endtime = Math.round((new Date()).getTime() / 1000) + video.length;
+
+                        video.save();
+
+                        socket.video = video;
+
+                        socket.emit("resetvideo", "");
+                    }
+                });
+            }
+            else {
+                console.log("No videos");
+                video.isplaying = 1;
+                video.hidden = 0;
+                video.starttime = Math.round((new Date()).getTime() / 1000);
+                video.endtime = Math.round((new Date()).getTime() / 1000) + video.length;
+
+                video.save();
+
+                socket.video = video;
+
+                socket.emit("resetvideo", "");
+            }
+        });
+
+        console.log("video changed");
+    }
 });
+
